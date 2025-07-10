ae4eec3dcd4af35b3394d7dce77187ffdbb77fdc
MuneebAhmad03 <muneeebahmad.03@gmail.com>
2025-07-10 23:37:36 +0500

    Add files via upload

---
+++ STACK.java
@@ -0,0 +1,19 @@
+import java.util.Stack;
+
+public class STACK {
+    public static void main(String[] args) {
+
+
+        Stack<String> stack = new Stack<String>();
+
+        stack.push("A");   //ADD IN STACK
+        stack.push("B");
+
+        stack.pop();   // REMOVE FROM STACK
+        
+        System.out.println(stack);
+        System.out.println(stack.peek());   //CHECK THE TOP OF THE STACK
+
+
+    }
+}
